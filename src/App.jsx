c11859e84021db3aedbf7bf94469b8d627b904ef
gkourouma <gkourouma2@gmail.com>
2025-03-26 16:34:50 -0500

    Create ternary operator to display information of added fighters.

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,138 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
 
 const App = () => {
-
   const handleAddFighter = (zombie) => {
     console.log(zombie);
     setTeam([...team, zombie]);
   };
 
   const [team, setTeam] = useState([]);
-  const [money,setMoney] = useState(100)
+  const [money, setMoney] = useState(100);
   const [zombieFighters, setZombieFighters] = useState([
     {
       id: 1,
-      name: 'Survivor',
+      name: "Survivor",
       price: 12,
       strength: 6,
       agility: 4,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png",
     },
     {
       id: 2,
-      name: 'Scavenger',
+      name: "Scavenger",
       price: 10,
       strength: 5,
       agility: 5,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png",
     },
     {
       id: 3,
-      name: 'Shadow',
+      name: "Shadow",
       price: 18,
       strength: 7,
       agility: 8,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png",
     },
     {
       id: 4,
-      name: 'Tracker',
+      name: "Tracker",
       price: 14,
       strength: 7,
       agility: 6,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png",
     },
     {
       id: 5,
-      name: 'Sharpshooter',
+      name: "Sharpshooter",
       price: 20,
       strength: 6,
       agility: 8,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png",
     },
     {
       id: 6,
-      name: 'Medic',
+      name: "Medic",
       price: 15,
       strength: 5,
       agility: 7,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png",
     },
     {
       id: 7,
-      name: 'Engineer',
+      name: "Engineer",
       price: 16,
       strength: 6,
       agility: 5,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png",
     },
     {
       id: 8,
-      name: 'Brawler',
+      name: "Brawler",
       price: 11,
       strength: 8,
       agility: 3,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png",
     },
     {
       id: 9,
-      name: 'Infiltrator',
+      name: "Infiltrator",
       price: 17,
       strength: 5,
       agility: 9,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png",
     },
     {
       id: 10,
-      name: 'Leader',
+      name: "Leader",
       price: 22,
       strength: 7,
       agility: 6,
-      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png",
     },
-  ]
-  ) 
+  ]);
 
   return (
     <div>
-    {zombieFighters.map((zombie) => (
-      <li key={zombie.id}>
-      <img src={zombie.img} alt="" />
-      <h2>{zombie.name}</h2>
-      <p>{zombie.price}</p>
-      <p>{zombie.strength}</p>
-      <p>{zombie.agility}</p>
-      <button onClick={() => handleAddFighter(zombie)} >Add</button>
-      </li>
-    ))}
-    <p>Money Amount: ${money}</p>
+      <h1>Zombie Fighters</h1>
+      <p>Money Amount: ${money}</p>
+      <p>Team Strength: </p>
+      <p>Team Agility: </p>
+      <p>
+        Team:
+        {team.length === 0
+          ? " Pick some Team members"
+          : team.map((zombie) => (
+              <li key={zombie.id}>
+                <img src={zombie.img} alt="" />
+                <h2>{zombie.name}</h2>
+                <p>Price: {zombie.price}</p>
+                <p>Strength: {zombie.strength}</p>
+                <p>Agility: {zombie.agility}</p>
+              </li>
+            ))}
+      </p>
 
-    <div>
-      <h1>My Team</h1>
-      <p></p>
+      <h2>Fighters</h2>
+      <ul>
+        {zombieFighters.map((zombie) => (
+          <li key={zombie.id}>
+            <img src={zombie.img} alt="" />
+            <h2>{zombie.name}</h2>
+            <p>Price: {zombie.price}</p>
+            <p>Strength: {zombie.strength}</p>
+            <p>Agility: {zombie.agility}</p>
+            <button onClick={() => handleAddFighter(zombie)}>Add</button>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <p></p>
+      </div>
     </div>
-  </div>
   );
-}
+};
 
-export default App
+export default App;
